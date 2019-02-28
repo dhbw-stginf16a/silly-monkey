8abb7d3ffe623571cde45ac411c227325ab99ff2
Daniel Schaefer <git@danielschaefer.me>
2019-02-28 16:35:35 +0100

    Implement FeinstaubAdapter for Alarm

---
+++ feinstaub-adapter/index.js
@@ -0,0 +1,24 @@
+const axios = require("axios")
+const express = require('express')
+const app = express()
+const port = 3000
+
+const feinstaubAlarmUrl = "https://www.stuttgart.de/feinstaubalarm/widget/xtrasmall"
+
+
+app.get('/isAlarm', async (req, res) => {
+  try {
+    const response = await axios(feinstaubAlarmUrl)
+    const isAlarm = response.data.indexOf("alarm-on") > -1
+    res.send({
+      "isAlarm": isAlarm
+    })
+  } catch (error) {
+    console.log(error)
+    res.send({
+      "error": error
+    })
+  }
+})
+
+app.listen(port, () => console.log(`Feinstaub adapter listening on port ${port}!`))
