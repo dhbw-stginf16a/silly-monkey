cc4326e78653b79b75a6df850e629d9e6f7ecad8
Alex S <al97sch@yahoo.de>
2019-03-01 16:36:12 +0100

    changes

---
+++ calendar-adapter/README.md
@@ -0,0 +1,19 @@
+example return object route calender-adapter/calender/todaysAppointments
+
+{ title: 'Calendar',
+  active: { calendar: true },
+  user: 'Schaefer, Alexandra',
+  events:
+   [ { '@odata.etag': 'W/" "',
+        id: 'someCalenderEntryId',
+        subject: 'Theorie Phase VI',
+        start: { dateTime: '2019-02-11T00:00:00.0000000', timeZone: 'UTC' },
+        end: { dateTime: '2019-05-06T00:00:00.0000000', timeZone: 'UTC' },
+        location: { 
+            displayName: 'DHBW Stuttgart',
+            locationType: 'default',
+            uniqueId: 'DHBW Stuttgart',
+            uniqueIdType: 'private' 
+        } 
+    } ] 
+}
--- calendar-adapter/routes/calendar.js
+++ calendar-adapter/routes/calendar.js
@@ -35,8 +35,54 @@ router.get('/', async function(req, res, next) {
       .get();
 
       parms.events = result.value;
-      console.log(parms.events);
       res.render('calendar', parms);
+    } catch (err) {
+      parms.message = 'Error retrieving events';
+      parms.error = { status: `${err.code}: ${err.message}` };
+      parms.debug = JSON.stringify(err.body, null, 2);
+      res.render('error', parms);
+    }
+
+  } else {
+    // Redirect to home
+    res.redirect('/');
+  }
+});
+
+/* GET /calendar */
+router.get('/todaysAppointments', async function(req, res, next) {
+  let parms = { title: 'Calendar', active: { calendar: true } };
+
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userName = req.cookies.graph_user_name;
+
+  if (accessToken && userName) {
+    parms.user = userName;
+
+    // Initialize Graph client
+    const client = graph.Client.init({
+      authProvider: (done) => {
+        done(null, accessToken);
+      }
+    });
+
+    // Set start of the calendar view to today at midnight
+    const start = new Date(new Date().setHours(0,0,0));
+    // Set end of the calendar view to 7 days from start
+    const end = new Date(new Date(start).setDate(start.getDate() + 1));
+
+    try {
+      // Get the first 10 events for the coming week
+      const result = await client
+      .api(`/me/calendarView?startDateTime=${start.toISOString()}&endDateTime=${end.toISOString()}`)
+      .top(10)
+      .select('subject,start,end,location')
+      .orderby('start/dateTime DESC')
+      .get();
+
+      parms.events = result.value;
+      console.log(parms.events);
+      res.send(parms);
     } catch (err) {
       parms.message = 'Error retrieving events';
       parms.error = { status: `${err.code}: ${err.message}` };
