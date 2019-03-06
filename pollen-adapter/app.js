49253ec7423d6de494e4cd408bf2059bb316c06c
Cathleen <ca.schmalfuss@live.de>
2019-03-06 11:55:45 +0100

    pollen mockup

---
+++ pollen-adapter/app.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let getPollen = require('./routes/getPollen');
+
+let app = express();
+
+const port = 5003;
+
+app.use('/getPollen', getPollen);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log('app listening on port 3000!');
+})
+
+module.exports = app;
---
+++ pollen-adapter/routes/getPollen.js
@@ -0,0 +1,24 @@
+let express = require('express');
+
+let router = express.Router();
+
+router.get('/', (req, res) => {
+    const pollen = req.pollen;
+    const place = req.place;
+
+    // send mock-up answer
+    result = {
+        'Hasel': {
+            'today': 0,
+            'tomorrow': 1,
+            'dayafter_to': 1,
+        },
+        'Erle': {
+            'today': 2,
+            'tomorrow': 2.5,
+            'dayafter-to': 1,
+        },
+    }
+
+    res.send(result);
+})
