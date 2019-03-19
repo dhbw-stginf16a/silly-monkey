ff13a85b7fd468e18c799a64569ca474d2db7466
Alex S <al97sch@yahoo.de>
2019-03-19 11:01:38 +0100

    more mock calendar data

--- calendar-adapter/routes/calendar.js
+++ calendar-adapter/routes/calendar.js
@@ -13,9 +13,12 @@ router.get('/getMockCalendar', async function(req, res, next) {
     // Set start of the calendar view to today at midnight
     const start1 = new Date(new Date().setHours(9,0,0));
     const start2 = new Date(new Date().setHours(13,0,0));
+    const start3 = new Date(new Date().setHours(15,0,0));
     // Set end of the calendar view to 7 days from start
     const end1 = new Date(new Date().setHours(10,0,0));
     const end2 = new Date(new Date().setHours(13,30,0));
+    const end3 = new Date(new Date().setHours(18,0,0));
+   
 
   parms.events = 
     [{
@@ -28,6 +31,11 @@ router.get('/getMockCalendar', async function(req, res, next) {
     start: start2,
     end: end2,
     location: "Office"
+    },
+    {subject: "Coffee with Melanie",
+    start: start3,
+    end: end3,
+    location: "Central Perk Coffee Shop"
     }];
 
     res.send(parms);
