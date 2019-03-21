e91344ed444660477bca21b637516e50e4a3b718
Paul Flachbart <paul.flachbart@hpe.com>
2019-03-21 14:47:21 +0100

    changed different endpoints to different values in request body

--- daily-overview-usecase/routes/getOverview.js
+++ daily-overview-usecase/routes/getOverview.js
@@ -12,88 +12,142 @@ const calenderAdapter = "http://calendar-adapter:5002/calendar";
 const calenderMeetings = "http://calendar-adapter:5002/calendar/getMockCalendar";
 const pollenAdapter = "http://pollen-adapter:5003/getPollen";
 
-router.get('/', async (req, response) => {
-
-    var locationResponse;
-    var trainResponse;
-    var trainHomeStation;
-    var homeLocation;
-    var feinstaubResponse;
-    var feinstaub;
-    var weatherResponse;
-    var weather;
-    var vvsResponse;
-    var vvsDepartures;
-    var trainInfo = "";
-    var feinstaubInfo = "";
-    var calenderMeetingsResponse;
-    var trafficResponse;
-    var trafficData;
-    var trafficInfo = "";
-    var raodResponse;
-    var favRoads;
-    try {
-        locationResponse = await axios(dbConnectionViaTriggerRouter + "/location");
-        trainResponse = await axios(dbConnectionViaTriggerRouter + "/homeStation");
-        raodResponse = await axios(dbConnectionViaTriggerRouter + "/favRoads");
-        favRoads = raodResponse.data.value.favRoads;
-        feinstaubResponse = await axios(feinstaubAdapter, {params: {"location": locationResponse.data.value.location}});
-        trainHomeStation = trainResponse.data.value.homeStation;
-        homeLocation = locationResponse.data.value.location;
-        feinstaub = feinstaubResponse.data.isAlarm;
-        vvsResponse = await axios(vvsAdapter + "?stationname=" + trainHomeStation);
-        vvsDepartures = vvsResponse.data;
-        trafficResponse = await axios(trafficAdapter + "?streets=" + favRoads);
-
-
-        //vvs
+router.post('/', async (req, response) => {
+    if(req.body.key == "overview") {
+        var locationResponse;
+        var trainResponse;
+        var trainHomeStation;
+        var homeLocation;
+        var feinstaubResponse;
+        var feinstaub;
+        var weatherResponse;
+        var weather;
+        var vvsResponse;
+        var vvsDepartures;
+        var trainInfo = "";
+        var feinstaubInfo = "";
+        var calenderMeetingsResponse;
+        var trafficResponse;
+        var trafficData;
+        var trafficInfo = "";
+        var raodResponse;
+        var favRoads;
+        try {
+            locationResponse = await axios(dbConnectionViaTriggerRouter + "/location");
+            trainResponse = await axios(dbConnectionViaTriggerRouter + "/homeStation");
+            raodResponse = await axios(dbConnectionViaTriggerRouter + "/favRoads");
+            favRoads = raodResponse.data.value.favRoads;
+            feinstaubResponse = await axios(feinstaubAdapter, {params: {"location": locationResponse.data.value.location}});
+            trainHomeStation = trainResponse.data.value.homeStation;
+            homeLocation = locationResponse.data.value.location;
+            feinstaub = feinstaubResponse.data.isAlarm;
+            vvsResponse = await axios(vvsAdapter + "?stationname=" + trainHomeStation);
+            vvsDepartures = vvsResponse.data;
+            trafficResponse = await axios(trafficAdapter + "?streets=" + favRoads);
 
 
-        for (let dep of vvsDepartures) {
-            if(dep.delay >= 4) {
-                trainInfo += "The train " + dep.number + " in direction " + dep.direction +" is delayed by " + dep.delay + " minutes.";
+            //vvs
+
+
+            for (let dep of vvsDepartures) {
+                if (dep.delay >= 4) {
+                    trainInfo += "The train " + dep.number + " in direction " + dep.direction + " is delayed by " + dep.delay + " minutes.";
+                }
             }
-        }
 
-        if(trainInfo === "") {
-            trainInfo += "There are no delayed trains. What a surprise!";
-        }
+            if (trainInfo === "") {
+                trainInfo += "There are no delayed trains. What a surprise!";
+            }
 
-        //feinstaub
-        if(feinstaub){
-            feinstaubInfo = "Also, there is currently Feinstaubalarm at your location, so you should consider taking the Train to fight against pollution. "
-        }else{
-            feinstaubInfo = "Also, since there is no Feinstaubalarm at your location, feel free to take the car and pollute the hell out of this air. "
-        }
+            //feinstaub
+            if (feinstaub) {
+                feinstaubInfo = "Also, there is currently Feinstaubalarm at your location, so you should consider taking the Train to fight against pollution. "
+            } else {
+                feinstaubInfo = "Also, since there is no Feinstaubalarm at your location, feel free to take the car and pollute the hell out of this air. "
+            }
 
-        //weather
-        var todayNow = new Date().toLocaleTimeString().toString();
-        todayNow = todayNow.replace(":/g", "");
-        todayNow = todayNow + "0000";
+            //weather
+            var todayNow = new Date().toLocaleTimeString().toString();
+            todayNow = todayNow.replace(":/g", "");
+            todayNow = todayNow + "0000";
 
-        weatherResponse = await axios.post(weatherAdapter, {
-                "time":todayNow,
-                "location":locationResponse.data.value.location,
-                "country":"de"
+            weatherResponse = await axios.post(weatherAdapter, {
+                "time": todayNow,
+                "location": locationResponse.data.value.location,
+                "country": "de"
             });
 
-        weather = weatherResponse.data.weather[0].description + " at " + (weatherResponse.data.main.temp - 273.15).toFixed(2) + "°C" ;
+            weather = weatherResponse.data.weather[0].description + " at " + (weatherResponse.data.main.temp - 273.15).toFixed(2) + "°C";
 
 
-        //verkehr
-        trafficData = trafficResponse.data;
-        console.log(trafficData);
+            //verkehr
+            trafficData = trafficResponse.data;
+            console.log(trafficData);
 
-        for (let traffic of trafficData) {
-            trafficInfo += traffic.message + ". ";
+            for (let traffic of trafficData) {
+                trafficInfo += traffic.message + ". ";
+            }
+
+            if (trafficInfo === "") {
+                trafficInfo += "There are no delays on your usual roads. ";
+            }
+            console.log(trafficInfo);
+
+
+            //calendar
+            try {
+                calenderMeetingsResponse = await axios(calenderMeetings);
+            } catch (error) {
+                console.log(error.message);
+                res.send({
+                    "error": error.message
+                });
+            }
+            var scheduledMeetings = calenderMeetingsResponse.data;
+
+
+            for (let meeting of scheduledMeetings.events) {
+                var meetingStart = new Date(meeting.start);
+                var meetingEnd = new Date(meeting.end);
+
+                var startTime = meetingStart.getHours() + ":" + meetingStart.getMinutes();
+                if (meetingStart.getMinutes() == 0) {
+                    startTime += "0";
+                }
+                var endTime = meetingEnd.getHours() + ":" + meetingEnd.getMinutes();
+                if (meetingEnd.getMinutes() == 0) {
+                    endTime += "0";
+                }
+
+                var meetingOverview = meeting.subject;
+
+                meetingOverview += " at " + startTime;
+                meetingOverview += "";
+                meetingOverview += " at ";
+                meetingOverview += meeting.location;
+                meetingOverview += ". ";
+                meetingOverview += "This meeting ends at " + endTime + ". ";
+
+            }
+
+
+        } catch (error) {
+            console.log(error.message);
+            res.send({
+                "error": error.message
+            });
         }
 
-        if(trafficInfo === "") {
-            trafficInfo += "There are no delays on your usual roads. ";
-        }
-        console.log(trafficInfo);
 
+        response.send("the weather at your location " + homeLocation +
+            " is currently " + weather +
+            ". At your preferred station " + trainHomeStation +
+            " " + trainInfo +
+            " " + trafficInfo +
+            feinstaubInfo +
+            "last but not least, today you've got the following meetings. " + meetingOverview);
 
+    }else if(req.body.key == "meetings"){
         //calendar
         try {
             calenderMeetingsResponse = await axios(calenderMeetings);
@@ -111,11 +165,11 @@ router.get('/', async (req, response) => {
             var meetingEnd = new Date(meeting.end);
 
             var startTime = meetingStart.getHours() + ":" + meetingStart.getMinutes();
-            if(meetingStart.getMinutes() == 0) {
+            if (meetingStart.getMinutes() == 0) {
                 startTime += "0";
             }
             var endTime = meetingEnd.getHours() + ":" + meetingEnd.getMinutes();
-            if(meetingEnd.getMinutes() == 0) {
+            if (meetingEnd.getMinutes() == 0) {
                 endTime += "0";
             }
 
@@ -131,141 +185,74 @@ router.get('/', async (req, response) => {
         }
 
 
-    } catch (error) {
-        console.log(error.message);
-        res.send({
-            "error": error.message
-        });
-    }
+        response.send("today you've got the following meetings. " + meetingOverview);
 
 
-    response.send("the weather at your location " + homeLocation +
-        " is currently " + weather +
-        ". At your preferred station " + trainHomeStation +
-        " " + trainInfo +
-        " " + trafficInfo +
-        feinstaubInfo +
-        "last but not least, today you've got the following meetings. " + meetingOverview);
+    }else if(req.body.key == "traffic"){
+        var trainResponse;
+        var trainHomeStation;
+        var homeLocation;
+        var vvsResponse;
+        var vvsDepartures;
+        var trainInfo = "";
+        var trafficResponse;
+        var trafficData;
+        var trafficInfo = "";
+        var raodResponse;
+        var favRoads;
+        try {
+            trainResponse = await axios(dbConnectionViaTriggerRouter + "/homeStation");
+            raodResponse = await axios(dbConnectionViaTriggerRouter + "/favRoads");
+            favRoads = raodResponse.data.value.favRoads;
+            trainHomeStation = trainResponse.data.value.homeStation;
+            vvsResponse = await axios(vvsAdapter + "?stationname=" + trainHomeStation);
+            vvsDepartures = vvsResponse.data;
+            trafficResponse = await axios(trafficAdapter + "?streets=" + favRoads);
 
 
-
-});
-
-
-router.get('/justMeetings', async (req, response) => {
-
-        //calendar
-    try {
-        calenderMeetingsResponse = await axios(calenderMeetings);
-    } catch (error) {
-        console.log(error.message);
-        res.send({
-            "error": error.message
-        });
-    }
-    var scheduledMeetings = calenderMeetingsResponse.data;
+            //vvs
 
 
-    for (let meeting of scheduledMeetings.events) {
-        var meetingStart = new Date(meeting.start);
-        var meetingEnd = new Date(meeting.end);
-
-        var startTime = meetingStart.getHours() + ":" + meetingStart.getMinutes();
-        if(meetingStart.getMinutes() == 0) {
-            startTime += "0";
-        }
-        var endTime = meetingEnd.getHours() + ":" + meetingEnd.getMinutes();
-        if(meetingEnd.getMinutes() == 0) {
-            endTime += "0";
-        }
-
-        var meetingOverview = meeting.subject;
-
-        meetingOverview += " at " + startTime;
-        meetingOverview += "";
-        meetingOverview += " at ";
-        meetingOverview += meeting.location;
-        meetingOverview += ". ";
-        meetingOverview += "This meeting ends at " + endTime + ". ";
-
-    }
-
-
-    response.send("today you've got the following meetings. " + meetingOverview);
-
-
-
-});
-
-router.get('/justTraffic', async (req, response) => {
-
-    var trainResponse;
-    var trainHomeStation;
-    var homeLocation;
-    var vvsResponse;
-    var vvsDepartures;
-    var trainInfo = "";
-    var trafficResponse;
-    var trafficData;
-    var trafficInfo = "";
-    var raodResponse;
-    var favRoads;
-    try {
-        trainResponse = await axios(dbConnectionViaTriggerRouter + "/homeStation");
-        raodResponse = await axios(dbConnectionViaTriggerRouter + "/favRoads");
-        favRoads = raodResponse.data.value.favRoads;
-        trainHomeStation = trainResponse.data.value.homeStation;
-        vvsResponse = await axios(vvsAdapter + "?stationname=" + trainHomeStation);
-        vvsDepartures = vvsResponse.data;
-        trafficResponse = await axios(trafficAdapter + "?streets=" + favRoads);
-
-
-        //vvs
-
-
-        for (let dep of vvsDepartures) {
-            if(dep.delay >= 4) {
-                trainInfo += "The train " + dep.number + " in direction " + dep.direction +" is delayed by " + dep.delay + " minutes.";
+            for (let dep of vvsDepartures) {
+                if(dep.delay >= 4) {
+                    trainInfo += "The train " + dep.number + " in direction " + dep.direction +" is delayed by " + dep.delay + " minutes.";
+                }
             }
+
+            if(trainInfo === "") {
+                trainInfo += "There are no delayed trains. What a surprise!";
+            }
+
+            //verkehr
+            trafficData = trafficResponse.data;
+            console.log(trafficData);
+
+            for (let traffic of trafficData) {
+                trafficInfo += traffic.message + ". ";
+            }
+
+            if(trafficInfo === "") {
+                trafficInfo += "There are no delays on your usual roads. ";
+            }
+            console.log(trafficInfo);
+
+
+
+        } catch (error) {
+            console.log(error.message);
+            res.send({
+                "error": error.message
+            });
         }
 
-        if(trainInfo === "") {
-            trainInfo += "There are no delayed trains. What a surprise!";
-        }
 
-        //verkehr
-        trafficData = trafficResponse.data;
-        console.log(trafficData);
-
-        for (let traffic of trafficData) {
-            trafficInfo += traffic.message + ". ";
-        }
-
-        if(trafficInfo === "") {
-            trafficInfo += "There are no delays on your usual roads. ";
-        }
-        console.log(trafficInfo);
+        response.send("At your preferred station " + trainHomeStation +
+            " " + trainInfo +
+            " " + trafficInfo);
 
 
-
-    } catch (error) {
-        console.log(error.message);
-        res.send({
-            "error": error.message
-        });
     }
 
-
-    response.send("At your preferred station " + trainHomeStation +
-        " " + trainInfo +
-        " " + trafficInfo);
-
-
-
 });
-
-
-
-
 
 module.exports = router;
