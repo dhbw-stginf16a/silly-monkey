080489c4726dc7ded8a6d665f6ec32633a89c6ee
Cathleen <ca.schmalfuss@live.de>
2019-03-27 15:43:46 +0100

    split up test cases for pollen adapter

--- pollen-adapter/test/getPollen.js
+++ pollen-adapter/test/getPollen.js
@@ -9,12 +9,16 @@ chai.use(chaiHttp);
 
 describe('Pollen Adapter', () => {
     describe('/GET pollen info', () => {
-        it('should GET the pollen info', async () => {
+        let pollenPref = [];
+        let regionPref = '';
+        before(async () => {
             const pollenPrefPromise = axios('http://trigger-router:5000/database/pollen');
             const regionPrefPromise = axios('http://trigger-router:5000/database/region');
-            const pollenPref = (await pollenPrefPromise).data.value.pollen;
-            const regionPref = (await regionPrefPromise).data.value.region;
+            pollenPref = (await pollenPrefPromise).data.value.pollen;
+            regionPref = (await regionPrefPromise).data.value.region;
+        });
 
+        it('should have status code 200', (done) => {
             chai.request(app)
                 .get(`/getPollen?pollen=${pollenPref.join(', ')}&place=${regionPref}`)
                 .end((err, res) => {
@@ -23,14 +27,54 @@ describe('Pollen Adapter', () => {
                     }
 
                     res.should.have.status(200);
+
+                    done();
+                });
+        });
+
+        it('should return an object', (done) => {
+            chai.request(app)
+                .get(`/getPollen?pollen=${pollenPref.join(', ')}&place=${regionPref}`)
+                .end((err, res) => {
+                    if (err) {
+                        console.log(err);
+                    }
+
                     res.body.should.be.an('object');
+
+                    done();
+                });
+        });
+
+        it('should return an object with keys corresponding to the user preferences', (done) => {
+            chai.request(app)
+                .get(`/getPollen?pollen=${pollenPref.join(', ')}&place=${regionPref}`)
+                .end((err, res) => {
+                    if (err) {
+                        console.log(err);
+                    }
+
                     for (let i = 0; i < pollenPref.length; i++) {
                         res.body.should.have.property(pollenPref[i]);
+                    }
+
+                    done();
+                });
+        });
+
+        it('should return an object with the necessary keys on the second level', (done) => {
+            chai.request(app)
+                .get(`/getPollen?pollen=${pollenPref.join(', ')}&place=${regionPref}`)
+                .end((err, res) => {
+                    if (err) {
+                        console.log(err);
                     }
 
                     Object.keys(res.body).forEach((key) => {
                         res.body[key].should.have.all.keys('today', 'tomorrow', 'dayafter_to');
                     });
+
+                    done();
                 });
         });
     });
